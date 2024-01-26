1e25090821b893520e91320953a6c4b9a739a938
rexplays11 <nikola.dedovic.3929@gmail.com>
2024-01-26 22:20:45 +0100

    Initial commit

---
+++ Biblioteka/src/test/java/com/IT355/Biblioteka/BibliotekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IT355.Biblioteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
